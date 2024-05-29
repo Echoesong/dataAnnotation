c9d2a3096cebe69ec327586607cc623cfda0419a
Echoesong <jayefelker@gmail.com>
2024-05-28 20:10:11 -0500

    first attempt at solution

--- decode.py
+++ decode.py
@@ -1,7 +1,50 @@
-lines = []
+import re
 
 with open("coding_qual_input.txt", "r") as file:
+    cipher = {}
+    pattern = r"\d"
     for line in file:
-        lines.append(line.strip())
+        digits = []
+        alphabet = []
+        middle = 0
+        for i, character in enumerate(line):
+            # Check if it's a number. If it is, append it to digits; if it's not, we've found the middle and set middle to i
+            if re.fullmatch(pattern, character):
+                digits.append(character)
+            else:
+                middle = i
+                break
+        for j, character in enumerate(line):
+            if j > middle:
+                alphabet.append(character)
+        finalDigits = int("".join(digits))
+        finalAlphabet = "".join(alphabet).strip()
+        cipher[finalDigits] = finalAlphabet
 
-print(lines)
+    # Below is setting up the pyramid
+    encodedDigits = sorted(cipher.keys())
+    digitsLength = len(encodedDigits)
+    incrementer = 0
+    countToAdd = incrementer + 1
+    pyramid = []
+    while incrementer < digitsLength:
+        if len(encodedDigits) == 0:
+            break
+        row = []
+        for n in range(countToAdd):
+            popped = encodedDigits.pop(0)
+            row.append(popped)
+        incrementer += 1
+        countToAdd = incrementer + 1
+        pyramid.append(row)
+    output = []
+    for array in pyramid:
+        toPopIn = array.pop()
+        output.append(toPopIn)
+    answers = [
+        cipher[answerNumber] for answerNumber in output if answerNumber in cipher
+    ]
+    print(answers)
+
+# I think I want to flag 'good digits'
+# So I need to write a function that will output
